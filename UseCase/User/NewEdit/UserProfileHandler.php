ccb196ca4ccbf82a5003127455c074001e94db8c
kingstar <admin@baks.dev>
2023-11-01 15:04:44 +0300

    Baks Development

--- UseCase/User/NewEdit/UserProfileHandler.php
+++ UseCase/User/NewEdit/UserProfileHandler.php
@@ -68,7 +68,7 @@ final class UserProfileHandler extends AbstractHandler
 
         try
         {
-            $command->getEvent() ? $this->preUpdate($command) : $this->prePersist($command);
+            $command->getEvent() ? $this->preUpdate($command, true) : $this->prePersist($command);
         }
         catch(DomainException $errorUniqid)
         {
