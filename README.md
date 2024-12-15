4bc58ff5b6fe7799bc774f5f2078090c846abc8a
kingstar <admin@baks.dev>
2024-12-16 00:16:55 +0300

    Baks Development

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # BaksDev Profile Users
 
-[![Version](https://img.shields.io/badge/version-7.1.20-blue)](https://github.com/baks-dev/users-profile-user/releases)
+[![Version](https://img.shields.io/badge/version-7.1.21-blue)](https://github.com/baks-dev/users-profile-user/releases)
 ![php 8.3+](https://img.shields.io/badge/php-min%208.3-red.svg)
 
 Модуль профилей пользователя
--- Type/Event/UserProfileEventUid.php
+++ Type/Event/UserProfileEventUid.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,8 +29,8 @@ use Symfony\Component\Uid\AbstractUid;
 
 final class UserProfileEventUid extends Uid
 {
-    public const TEST = '0188a9a8-54c9-716f-9297-94a6348291a5';
-    
-	public const TYPE = 'user_profile_event';
+    public const string TEST = '0188a9a8-54c9-716f-9297-94a6348291a5';
+
+    public const string TYPE = 'user_profile_event';
 
 }
