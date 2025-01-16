e80a099fa007376c44d046424c8fa074b294930a
kingstar <admin@baks.dev>
2025-01-17 00:41:17 +0300

    Baks Development

--- Resources/config/messenger.php
+++ Resources/config/messenger.php
@@ -31,7 +31,7 @@ return static function (FrameworkConfig $framework) {
 
     $messenger
         ->transport('users-profile-user')
-        ->dsn('redis://%env(REDIS_PASSWORD)%@%env(REDIS_HOST)%:%env(REDIS_PORT)%?dbindex=%env(REDIS_TABLE)&auto_setup=true')
+        ->dsn('redis://%env(REDIS_PASSWORD)%@%env(REDIS_HOST)%:%env(REDIS_PORT)%?dbindex=%env(REDIS_TABLE)%&auto_setup=true')
         ->options(['stream' => 'users-profile-user'])
         ->failureTransport('failed-users-profile-user')
         ->retryStrategy()
