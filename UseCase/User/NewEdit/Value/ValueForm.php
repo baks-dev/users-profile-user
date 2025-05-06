52a42ec6f6e302bce9c4f5f0e3ab89219faa36ba
MichelAngelo <baksdevelopmen@gmail.com>
2025-05-06 12:02:27 +0300

    BaksDevelopment

--- UseCase/User/NewEdit/Value/ValueForm.php
+++ UseCase/User/NewEdit/Value/ValueForm.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,7 +24,6 @@
 namespace BaksDev\Users\Profile\UserProfile\UseCase\User\NewEdit\Value;
 
 use BaksDev\Core\Services\Fields\FieldsChoice;
-use BaksDev\Users\Profile\UserProfile\Repository\FieldValueForm\FieldValueFormDTO;
 use BaksDev\Users\Profile\UserProfile\Repository\FieldValueForm\FieldValueFormInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -76,8 +75,6 @@ final class ValueForm extends AbstractType
                     {
                         $fieldType = $this->fieldsChoice->getChoice($field->getType());
 
-
-
                         if($fieldType)
                         {
 
@@ -99,7 +96,6 @@ final class ValueForm extends AbstractType
                             }
 
                             $form->add('value', $fieldType->form(), $option);
-
 
 
 //                            $form->add
