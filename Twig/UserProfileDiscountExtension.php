20d4208db7215d45451a1f7cb7d6d38400b6a85b
MichelAngelo <baksdevelopmen@gmail.com>
2025-05-09 22:11:27 +0300

    BaksDevelopment

--- Twig/UserProfileDiscountExtension.php
+++ Twig/UserProfileDiscountExtension.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -40,17 +40,22 @@ final class UserProfileDiscountExtension extends AbstractExtension
 	/** Пользовательская скидка */
 	public function discount(Environment $twig, $number): mixed
 	{
+        if(empty($number))
+        {
+            return false;
+        }
+
 		$globals = $twig->getGlobals();
 
 		if(array_key_exists('baks_profile', $globals))
 		{
 			if(array_key_exists('profile_discount', $globals['baks_profile']))
 			{
-				if(!empty($globals['baks_profile']['profile_discount']))
+                if(false === empty($globals['baks_profile']['profile_discount']))
 				{
 					$discount = $globals['baks_profile']['profile_discount'];
-					
-					return $number - ($number / 100 * $discount);
+
+                    return $number - ($number / 100 * $discount);
 				}
 			}
 		}
